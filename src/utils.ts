65ad6bd692f7f962a288ec88a3a2cd94854a27d8
shlroland <shlroland1995@gmail.com>
2021-07-18 10:08:30 +0800

    :fire: (utils) remove comments

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,6 @@
 import { Ref, ComponentPublicInstance } from 'vue'
 import { toPath as _toPath } from 'lodash-es'
 export { get, set, merge, setWith } from 'lodash-es'
-// export { default as merge } from 'lodash.merge'
-// export { default as setWith } from 'lodash.setwith'
 
 export const isUnmounted = (fieldRef: Ref) => fieldRef.value === null
 
@@ -16,10 +14,6 @@ export const isAllUnmounted = (fieldRefs?: Set<Ref>) => {
 export const toPath = _toPath
 
 export const toPathString = (path: any) => toPath(path).join('.')
-
-// export const set = _set
-
-// export const get = _get
 
 export type FieldNode = HTMLElement | null | ComponentPublicInstance
 export const getDOMNode = (value: FieldNode) => {
